c5e911bc1a1a5b03c90f05e8ba18d967c269cb2a
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-13 05:30:39 +0200

    Refactor app structure:
    move MyApp and RecognitionTextWidget to separate files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image2text/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: const RecognitionTextWidget());
-  }
-}
-
-class RecognitionTextWidget extends StatefulWidget {
-  const RecognitionTextWidget({super.key});
-  @override
-  State<RecognitionTextWidget> createState() => _RecognitionTextWidgetState();
-}
-
-class _RecognitionTextWidgetState extends State<RecognitionTextWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
 }
