e485adc47d7ed9fcfa677e28339310c8fc7ca531
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-14 09:50:47 +0200

    Add dependencies:
    -  camera.
    -  image.
    -  path_provider.
    and reorder imports in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:image2text/my_app.dart';
 import 'package:image2text/features/recognition_text/services/permission_handle/request_camera_permission.dart';
+import 'package:image2text/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
