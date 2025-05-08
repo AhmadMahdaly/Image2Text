6386c7e581e9b67783f697143d8164fe6c5b88d3
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-08 05:37:19 +0300

    Refactor main.dart and my_app.dart to improve structure and register Hive adapters

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image2text/features/recognition_text/cubit/recognition_text_cubit.dart';
+import 'package:image2text/core/cubit/recognition_text_cubit.dart';
+import 'package:image2text/core/services/unfocus_scope.dart';
 import 'package:image2text/features/recognition_text/view/recognition_text_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<RecognitionTextCubit>(
-      create: (context) => RecognitionTextCubit(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: RecognitionTextScreen(),
+    return GestureDetector(
+      onTap: () => unfocusScope(context),
+      child: BlocProvider<TextCubit>(
+        create: (context) => TextCubit(),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: RecognitionTextScreen(),
+        ),
       ),
     );
   }
